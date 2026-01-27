3a848e1c213ff840ff7c5a43d0cf833905784b62
Sebi19 <s.schreitter@gmx.at>
2026-01-27 15:50:47 +0100

    Initial commit backend

---
+++ backend/src/main/java/com/election/backend/AnonymousElectionApplication.java
@@ -0,0 +1,13 @@
+package com.election.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousElectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousElectionApplication.class, args);
+	}
+
+}
